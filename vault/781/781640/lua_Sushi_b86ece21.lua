17f019d9ae03301598088c19def03d36b08b10cd
furious-vaults <diegohenriquewcl@hotmail.com>
2026-04-05 21:50:03 -0300

    up

---
+++ vault/781/781640/lua_Sushi_b86ece21.lua
@@ -0,0 +1,10 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+
+-- MAIN APPLICATION
+addappid(781640) -- Run Jump Fail
+
+-- MAIN APP DEPOTS
+addappid(781641, 1, "bac6f3bcc1085c3ea52ce43b04003856dc9924fe3c637a6c6c4897f384a54af1") -- Run Jump Fail Content
+setManifestid(781641, "8755816494643551252", 0)
---
+++ vault/794/794860/lua_Sushi_164eccdd.lua
@@ -0,0 +1,16 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(794860) -- eSail Sailing Simulator
+
+-- MAIN APP DEPOTS
+addappid(794861, 1, "35a0131cbd83b246f13a3abe94f1b3b58934ac648918f770493d7eda3f9c995d") -- eSail Windows
+setManifestid(794861, "5422382513366781777", 0)
+addappid(794863, 1, "8a9a9941ba0c8bc7af420c385ae2305a89dcc8fd3559a664c2ab082a8cab6001") -- eSail Mac
+setManifestid(794863, "6959571839235260485", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1121600) -- eSail Man Overboard (MOB)
+addappid(1853470) -- eSail Pro
